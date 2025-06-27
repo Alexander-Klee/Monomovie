6ae9993b7012b1670572a76c045667058d564f5b
Alexander Klee <alexander.m.klee@gmail.com>
2025-06-27 14:44:33 +0200

    feat: search by monetization

--- src/main/kotlin/de/amklee/Main.kt
+++ src/main/kotlin/de/amklee/Main.kt
@@ -1,11 +1,23 @@
 package de.amklee
 
+val bannedTypes = setOf("BUY", "RENT")
 
 suspend fun main() {
     val justWatch = JustWatch(country = "DE", language = "en")
 
-    val searchResult = justWatch.search("The Matrix")
+    val searchResult = justWatch.search("12 angry men")
 
-    println(searchResult)
+//    for (entry in searchResult) {
+////        println(entry)
+//
+//    }
+
+    val bestFitMovie = searchResult[0]
+    println("Best fit movie: ${bestFitMovie.content?.title}")
+    val offers = bestFitMovie.offers?.filter { it.monetizationType !in bannedTypes } ?: emptyList()
+
+    for (offer in offers) {
+        println("Found offer: ${offer.`package`?.clearName} at ${offer.standardWebURL}, presentationType: ${offer.presentationType}, monetizationType: ${offer.monetizationType}")
+    }
 
 }
