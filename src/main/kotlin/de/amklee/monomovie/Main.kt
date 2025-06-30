3ac4d599885d83a9ef6dfc1dc6862174b299bd68
Alexander Klee <alexander.m.klee@gmail.com>
2025-06-30 18:16:54 +0200

    feat: optimize mobile UI

--- src/main/kotlin/de/amklee/monomovie/Main.kt
+++ src/main/kotlin/de/amklee/monomovie/Main.kt
@@ -40,10 +40,27 @@ fun htmlTemplate(title: String, body: String, nav: String): String {
             </style>
         </head>
         <body>
+            <button class="menu-button" onclick="showMenu()">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z"/></svg>
+            </button>
+            <script>
+                function showMenu() {
+                    const nav = document.querySelector('nav');
+                    nav.classList.toggle('nav-visible');
+                    document.body.classList.toggle('menu-open', nav.classList.contains('nav-visible'));
+                }
+                function closeMenu() {
+                        const nav = document.querySelector('nav');
+                        if (nav.classList.contains('nav-visible')) {
+                            nav.classList.remove('nav-visible');
+                            document.body.classList.remove('menu-open');
+                        }
+                }
+            </script>
             <nav>
                 $nav
             </nav>
-            <main>
+            <main onclick="closeMenu()">
                 $body
             </main>
         </body>
