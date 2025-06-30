0e4ff3aa88ab4061a431489959b43be3aafba4e6
Alexander Klee <alexander.m.klee@gmail.com>
2025-06-30 17:01:17 +0200

    chore: factor CachedMovies into separate file

--- src/main/kotlin/de/amklee/monomovie/Main.kt
+++ src/main/kotlin/de/amklee/monomovie/Main.kt
@@ -10,13 +10,11 @@ import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.util.*
-import kotlinx.serialization.json.Json
 import org.intellij.lang.annotations.Language
 import java.io.FileNotFoundException
 import kotlin.io.path.Path
 import kotlin.io.path.exists
 import kotlin.io.path.readText
-import kotlin.io.path.writeText
 
 object Resources {
     val style: String by lazy {
@@ -30,74 +28,6 @@ object Resources {
     }
 }
 
-object CachedMovies {
-    data class Movie(val mediaEntry: MediaEntry, var isBookmarked: Boolean, val cacheDate: Long)
-
-    private val cache = mutableMapOf<String, Movie>()
-
-    private val bookmarksFile = Path("bookmarks.json")
-    private var bookmarks: MutableSet<String> = loadBookmarks()
-
-
-    // Load bookmarks from file at startup
-    private fun loadBookmarks(): MutableSet<String> {
-        return if (bookmarksFile.exists()) {
-            try {
-                Json.decodeFromString<Set<String>>(bookmarksFile.readText()).toMutableSet()
-            } catch (_: Exception) {
-                mutableSetOf()
-            }
-        } else {
-            mutableSetOf()
-        }
-    }
-
-    private fun saveBookmarks() {
-        bookmarksFile.writeText(Json.encodeToString(bookmarks))
-    }
-
-    suspend fun get(id: String): Movie?{
-        // TODO: invalidate cache entry, if older than ... (remember to keep isBookmarked state)
-        return cache[id] ?: run {
-            val jw = JustWatch(country = "DE", language = "en")
-            val details = jw.details(id)
-            if (details != null) {
-                val movie = Movie(mediaEntry = details, isBookmarked = id in bookmarks, cacheDate = System.currentTimeMillis())
-                cache[id] = movie
-                return@run movie
-            }
-            null
-        }
-    }
-
-    fun toggleBookmark(id: String): Movie? {
-        val movie = cache[id] ?: return null
-        movie.isBookmarked = !movie.isBookmarked
-        if (movie.isBookmarked) {
-            bookmarks.add(id)
-        } else {
-            bookmarks.remove(id)
-        }
-        saveBookmarks()
-        return movie
-    }
-
-    suspend fun search(title: String, numResults: Int = 4): List<Movie> {
-        val jw = JustWatch(country = "DE", language = "en")
-        val searchResults = jw.search(title = title, numResults)
-        return searchResults.map { mediaEntry ->
-            val isBookmarked = cache[mediaEntry.id]?.isBookmarked ?: false
-
-            val movie = Movie(mediaEntry = mediaEntry, isBookmarked = isBookmarked, cacheDate = System.currentTimeMillis())
-            if (mediaEntry.id != null) cache[mediaEntry.id] = movie
-
-            movie
-        }
-    }
-
-    suspend fun getBookmarkedMovies(): List<Movie> = bookmarks.mapNotNull { id -> get(id) }
-}
-
 val bannedTypes = setOf("BUY", "RENT")
 
 fun htmlTemplate(title: String, body: String, nav: String): String {
@@ -106,6 +36,7 @@ fun htmlTemplate(title: String, body: String, nav: String): String {
         <html lang="en">
         <head>
             <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
             <title>$title</title>
             <style>
                 ${Resources.styleAutoUpdate}
@@ -259,7 +190,6 @@ fun Route.miscRoutes() {
             )
         )
     }
-
     get("/search") {
         val title = call.request.queryParameters["title"]?.escapeHTML()
         val numResults = call.request.queryParameters["num"]?.toIntOrNull() ?: 4
@@ -285,7 +215,6 @@ fun Route.miscRoutes() {
 
         call.respond(HttpStatusCode.OK)
     }
-
     get("/bookmarks") {
         call.respondText(
             contentType = ContentType.parse("text/html"),
---
+++ src/main/kotlin/de/amklee/monomovie/Movie.kt
@@ -0,0 +1,76 @@
+package de.amklee.monomovie
+
+import kotlinx.serialization.json.Json
+import kotlin.io.path.Path
+import kotlin.io.path.exists
+import kotlin.io.path.readText
+import kotlin.io.path.writeText
+
+
+object CachedMovies {
+    data class Movie(val mediaEntry: MediaEntry, var isBookmarked: Boolean, val cacheDate: Long)
+
+    private val cache = mutableMapOf<String, Movie>()
+
+    private val bookmarksFile = Path("bookmarks.json")
+    private var bookmarks: MutableSet<String> = loadBookmarks()
+
+
+    // Load bookmarks from file at startup
+    private fun loadBookmarks(): MutableSet<String> {
+        return if (bookmarksFile.exists()) {
+            try {
+                Json.decodeFromString<Set<String>>(bookmarksFile.readText()).toMutableSet()
+            } catch (_: Exception) {
+                mutableSetOf()
+            }
+        } else {
+            mutableSetOf()
+        }
+    }
+
+    private fun saveBookmarks() {
+        bookmarksFile.writeText(Json.encodeToString(bookmarks))
+    }
+
+    suspend fun get(id: String): Movie?{
+        // TODO: invalidate cache entry, if older than ... (remember to keep isBookmarked state)
+        return cache[id] ?: run {
+            val jw = JustWatch(country = "DE", language = "en")
+            val details = jw.details(id)
+            if (details != null) {
+                val movie = Movie(mediaEntry = details, isBookmarked = id in bookmarks, cacheDate = System.currentTimeMillis())
+                cache[id] = movie
+                return@run movie
+            }
+            null
+        }
+    }
+
+    fun toggleBookmark(id: String): Movie? {
+        val movie = cache[id] ?: return null
+        movie.isBookmarked = !movie.isBookmarked
+        if (movie.isBookmarked) {
+            bookmarks.add(id)
+        } else {
+            bookmarks.remove(id)
+        }
+        saveBookmarks()
+        return movie
+    }
+
+    suspend fun search(title: String, numResults: Int = 4): List<Movie> {
+        val jw = JustWatch(country = "DE", language = "en")
+        val searchResults = jw.search(title = title, numResults)
+        return searchResults.map { mediaEntry ->
+            val isBookmarked = cache[mediaEntry.id]?.isBookmarked ?: false
+
+            val movie = Movie(mediaEntry = mediaEntry, isBookmarked = isBookmarked, cacheDate = System.currentTimeMillis())
+            if (mediaEntry.id != null) cache[mediaEntry.id] = movie
+
+            movie
+        }
+    }
+
+    suspend fun getBookmarkedMovies(): List<Movie> = bookmarks.mapNotNull { id -> get(id) }
+}
