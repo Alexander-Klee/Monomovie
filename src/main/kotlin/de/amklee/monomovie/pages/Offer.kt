62704b5500e838fc31f09e92fe81e7564313063b
Alexander Klee <alexander.m.klee@gmail.com>
2025-10-04 18:10:04 +0200

    feat: nicely put offers into a table

--- src/main/kotlin/de/amklee/monomovie/pages/Offer.kt
+++ src/main/kotlin/de/amklee/monomovie/pages/Offer.kt
@@ -3,11 +3,32 @@ package de.amklee.monomovie.pages
 import de.amklee.monomovie.CachedMovies
 import de.amklee.monomovie.Offer
 import de.amklee.monomovie.components.MovieItem
+import de.amklee.monomovie.components.OfferList
 import kotlinx.html.FlowContent
-import kotlinx.html.a
 import kotlinx.html.div
 import kotlinx.html.h1
 import kotlinx.html.p
+import kotlinx.html.table
+import kotlinx.html.td
+import kotlinx.html.tr
+
+private val monetizationTypes = setOf("Flatrate", "Rent", "Buy", "Free")
+
+private fun FlowContent.offerTable(offers: List<Offer>) {
+    table {
+        for (type in monetizationTypes) {
+            tr {
+                val typeOffers = offers.filter { it.monetizationType.equals(type, ignoreCase = true) }
+                if (typeOffers.isNotEmpty()) {
+                    td(classes = "offerType-td") { +type }
+                    td(classes = "offer-td") {
+                        OfferList(typeOffers)
+                    }
+                }
+            }
+        }
+    }
+}
 
 fun FlowContent.OfferPage(movie: CachedMovies.Movie, offers: Map<String, List<Offer>>) {
     div {
@@ -18,15 +39,7 @@ fun FlowContent.OfferPage(movie: CachedMovies.Movie, offers: Map<String, List<Of
             if (offers.isEmpty()) {
                 p { +"No offers found" }
             } else {
-                for (offer in offers) {
-                    p {
-                        +(offer.`package`?.clearName + " - " + offer.monetizationType + " - " + (offer.retailPrice?.let { "$it ${offer.currency}" } ?: "Free"))
-                    }
-                    a {
-                        href = offer.standardWebURL ?: ""
-                        +(offer.standardWebURL ?: "No link")
-                    }
-                }
+                offerTable(offers)
             }
         }
     }
