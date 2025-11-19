0bc8a932047f3eba8911866de999adcc85b5c13a
Alexander Klee <alexander.m.klee@gmail.com>
2025-11-19 15:23:53 +0100

    style: cleanup jellyfin client

--- src/main/kotlin/de/amklee/monomovie/JellyfinClient.kt
+++ src/main/kotlin/de/amklee/monomovie/JellyfinClient.kt
@@ -1,13 +1,17 @@
 package de.amklee.monomovie
 import io.gitlab.jfronny.commons.logger.SystemLoggerPlus
+import io.ktor.http.URLBuilder
+import io.ktor.http.appendPathSegments
+import io.ktor.http.parseQueryString
 import org.jellyfin.sdk.api.client.extensions.itemsApi
 import org.jellyfin.sdk.createJellyfin
 import org.jellyfin.sdk.model.ClientInfo
 import org.jellyfin.sdk.model.DeviceInfo
 import org.jellyfin.sdk.model.api.BaseItemDto
 import org.jellyfin.sdk.model.api.ItemFields
-import java.nio.charset.CoderMalfunctionError
-import javax.crypto.IllegalBlockSizeException
+import java.io.IOException
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
 import kotlin.time.Clock
 import kotlin.time.Duration.Companion.hours
 import kotlin.time.ExperimentalTime
@@ -15,6 +19,8 @@ import kotlin.time.Instant
 
 @OptIn(ExperimentalTime::class)
 object JellyfinClient {
+    private val log = SystemLoggerPlus.forName("MMV/Jellyfin")
+
     private val jellyfin = createJellyfin {
         clientInfo = ClientInfo(name = "monomovie", version = "1.0.0",)
         deviceInfo = DeviceInfo("monomovie", "monomovie")
@@ -22,7 +28,7 @@ object JellyfinClient {
 
     private val api by lazy {
         jellyfin.createApi(
-            baseUrl = "https://jellyfin.amklee.de",
+            baseUrl = System.getenv("MMV_JELLYFIN_HOST"),
             accessToken = System.getenv("MMV_JELLYFIN_TOKEN")
         )
     }
@@ -35,7 +41,7 @@ object JellyfinClient {
         }
         try {
             val items = api.itemsApi.getItems(recursive = true, fields = setOf(ItemFields.PROVIDER_IDS))
-            if (items.status != 200) throw CoderMalfunctionError(IllegalBlockSizeException())
+            if (items.status != 200) throw IOException("Unexpected return code: ${items.status}")
             cache = CacheEntry(
                 tmdb = items.content.items.mapNotNull {
                     val id = it.providerIds?.get("Tmdb") ?: return@mapNotNull null
@@ -48,17 +54,16 @@ object JellyfinClient {
             )
             return cache!!
         } catch (e: Throwable) {
-            SystemLoggerPlus.forName("Guttas").error("Oh noes!!!", e)
-            return cache!!
+            log.error("Could not fetch items", e)
+            return cache ?: CacheEntry(emptyMap(), emptyMap())
         }
     }
 
-    // FIXME: crashes when jellyfin not reachable
-    // FIXME: cleanup code
-
     private fun BaseItemDto.actualUrl(): String {
-        return "${api.baseUrl}/web/#/details?id=${this.id}&serverId=${this.serverId}"
+        return "${api.baseUrl}/web/#/details?id=${this.id.toString().urlEncode()}&serverId=${this.serverId?.urlEncode()}"
     }
+
+    private fun String.urlEncode() = URLEncoder.encode(this, StandardCharsets.UTF_8)
 
     suspend fun findTmdbOnJellyfin(tmdbId: String) = getItems().tmdb[tmdbId]
     suspend fun findImdbOnJellyfin(imdbId: String) = getItems().imdb[imdbId]
