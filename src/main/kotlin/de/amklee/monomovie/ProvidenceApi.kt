10f2474f7f18c7070a678bd5dbd312c7f08fa3f6
Alexander Klee <alexander.m.klee@gmail.com>
2026-01-26 17:01:45 +0100

    perf: parallelize colour resolution from tmdb

--- src/main/kotlin/de/amklee/monomovie/ProvidenceApi.kt
+++ src/main/kotlin/de/amklee/monomovie/ProvidenceApi.kt
@@ -1,6 +1,9 @@
 package de.amklee.monomovie
 
 import io.ktor.http.*
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 
@@ -8,6 +11,8 @@ import kotlinx.serialization.json.Json
  * https://providence.jfronny.dev/?page=about
  */
 object ProvidenceApi {
+    const val endpoint = "https://providence.jfronny.dev/"
+
     @Serializable
     data class WheelOption(
         val id: String?,
@@ -29,20 +34,24 @@ object ProvidenceApi {
         val actions: List<WheelAction>,
     )
 
-    suspend fun createWheel(entries: List<Pair<CachedMovies.Movie, Int>>): Url {
+    suspend fun createWheel(entries: List<Pair<CachedMovies.Movie, Int>>): Url = coroutineScope {
         val config = WheelConfig(
             hash = null,
-            options = entries.map { (movie, weight) -> WheelOption(
-                id = movie.mediaEntry.id!!,
-                label = movie.mediaEntry.content?.title ?: "Unknown Title",
-                weight = weight,
-                color = TmColour[movie],
-            ) },
+            options = entries.map { (movie, weight) ->
+                async {
+                    WheelOption(
+                        id = movie.mediaEntry.id!!,
+                        label = movie.mediaEntry.content?.title ?: "Unknown Title",
+                        weight = weight,
+                        color = TmColour[movie],
+                    )
+                }
+            }.awaitAll(),
             actions = listOf(
                 WheelAction("View Offers", "$hostname/offers/{id}")
             ),
         )
-        return URLBuilder("https://providence.jfronny.dev/").apply {
+        return@coroutineScope URLBuilder(endpoint).apply {
             parameters.append("config", Json.encodeToString(config))
         }.build()
     }
