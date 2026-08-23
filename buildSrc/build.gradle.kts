93013b49fab1dc6d66d5608300de264ac8fd4cc7
JFronny <git@akbvopenfl1.hopto.org>
2026-08-23 16:46:12 +0200

    style: integrate ktlint

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,4 +10,5 @@ repositories {
 dependencies {
     implementation("com.squareup:kotlinpoet:2.3.0")
     implementation("com.google.javascript:closure-compiler:v20260513")
+    implementation("org.jetbrains.kotlin.jvm:org.jetbrains.kotlin.jvm.gradle.plugin:2.4.10")
 }
---
+++ buildSrc/src/main/kotlin/ktlint.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+    kotlin("jvm")
+}
+
+repositories {
+    mavenCentral()
+}
+
+val ktlint = configurations.register("ktlint")
+
+dependencies {
+    ktlint("com.pinterest.ktlint:ktlint-cli:1.8.0")
+}
+
+val outputDir = project.layout.buildDirectory.dir("reports/ktlint/")
+val inputFiles = fileTree("src") { include("**/*.kt") } + fileTree("buildSrc/src") { include("**/*.kt") }
+val editorconfig = rootProject.file(".editorconfig").absolutePath
+
+tasks {
+    val ktlintRun = register("ktlintRun", JavaExec::class) {
+        group = "verification"
+        inputs.files(inputFiles)
+        outputs.dir(outputDir)
+        mainClass = "com.pinterest.ktlint.Main"
+        classpath(ktlint)
+        args = listOf("--editorconfig=$editorconfig", "src/**/*.kt", "--reporter=plain?group_by_file", "--reporter=gitlab,output=${outputDir.get().asFile.absolutePath}/ktlint.json")
+        jvmArgs = listOf("--add-opens", "java.base/java.lang=ALL-UNNAMED")
+    }
+
+    val ktlintFormat = register("ktlintFormat", JavaExec::class) {
+        group = "verification"
+        inputs.files(inputFiles)
+        outputs.dir(outputDir)
+        mainClass = "com.pinterest.ktlint.Main"
+        classpath(ktlint)
+        args = listOf("--editorconfig=$editorconfig", "-F", "src/**/*.kt")
+        jvmArgs = listOf("--add-opens", "java.base/java.lang=ALL-UNNAMED")
+    }
+
+    val lint = register("lint") {
+        group = "verification"
+        dependsOn(ktlintRun)
+    }
+
+    check { dependsOn(lint) }
+    compileKotlin { dependsOn(ktlintFormat) }
+}
